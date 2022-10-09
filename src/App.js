a6dd2778ac128a3ea30db42c37fb81b7db5c682c
Nathan Masters <nate@192.168.1.219>
2022-10-09 21:48:31 +0100

    l

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import StartScreen from './components/StartScreen';
 import NamesScreen from './components/NamesScreen';
@@ -11,23 +11,30 @@ import DrinkScreen from './components/DrinkScreen';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<StartScreen />}></Route>
-      <Route exact path='/names' element={<NamesScreen />}></Route>
-      <Route exact path='/ingredients' element={<IngredientsScreen />}></Route>
-      <Route exact path='/rules' element={<Rules />}></Route>
-      <Route
-        exact
-        path='/challenge/:ingredient'
-        element={<ChallengeScreen />}
-      ></Route>
-      <Route
-        exact
-        path='/lemonade-challenge'
-        element={<LemonadeChallenge />}
-      ></Route>
-      <Route exact path='/drink' element={<DrinkScreen />}></Route>
-    </Routes>
+    <h1>hello</h1>
+    // <Router>
+    //   <Routes>
+    //     <Route path='/' element={<StartScreen />}></Route>
+    //     <Route exact path='/names' element={<NamesScreen />}></Route>
+    //     <Route
+    //       exact
+    //       path='/ingredients'
+    //       element={<IngredientsScreen />}
+    //     ></Route>
+    //     <Route exact path='/rules' element={<Rules />}></Route>
+    //     <Route
+    //       exact
+    //       path='/challenge/:ingredient'
+    //       element={<ChallengeScreen />}
+    //     ></Route>
+    //     <Route
+    //       exact
+    //       path='/lemonade-challenge'
+    //       element={<LemonadeChallenge />}
+    //     ></Route>
+    //     <Route exact path='/drink' element={<DrinkScreen />}></Route>
+    //   </Routes>
+    // </Router>
   );
 }
 
