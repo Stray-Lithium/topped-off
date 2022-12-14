481f0af3b8ef9185eac640cb6127b9bf5d3b5526
Naglis Rimsa <naglis22@gmail.com>
2022-12-14 16:23:33 +0200

    updated content

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ sagaMiddleware.run(routeSaga);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<HashRouter basename='/topped-off/'>
+		<HashRouter basename='/'>
 			<Provider store={store}>
 				<App />
 			</Provider>
