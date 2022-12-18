51501e24e27c5b9edbf71ee425f1f72bb933fa15
Nathan Masters <nate@Nathans-MBP.broadband>
2022-12-18 20:24:23 +0000

    deploy

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ sagaMiddleware.run(routeSaga);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename='/'>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
